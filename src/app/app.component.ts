57976e5a683e1e1a9e91b8cd77f68e2698711d68
gorgant <gorgant@hotmail.com>
2017-11-21 06:18:27 -0500

    Implement my version of "routing"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  showRecipe = true;
+
+  onRecipeClick(){
+    this.showRecipe = true;
+  }
+
+  onShoppingClick(){
+    this.showRecipe = false;
+  }
 
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  @Output('recipeClicked') recipeClicked = new EventEmitter<boolean>();
+  @Output('shoppingListClicked') shoppingListClicked = new EventEmitter<boolean>();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeClick(){
+    this.recipeClicked.emit(true);
+    console.log("recipe clicked");
+  }
+
+  onShoppingListClick(){
+    this.shoppingListClicked.emit(true);
+    console.log("shopping clicked");
   }
 
 }
