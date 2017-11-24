df97e21b53dfd8b9f074debcd14e58b11b7437a8
gorgant <gorgant@hotmail.com>
2017-11-24 14:55:38 -0500

    Implement working services for recipes

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from '../recipe.model';
-import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -9,16 +8,14 @@ import { RecipeService } from '../recipe.service';
 })
 export class RecipeDetailComponent implements OnInit {
 
-  recipe: Recipe;
+  @Input() recipe: Recipe;
 
-  constructor(private recipeService: RecipeService) {
+  constructor() {
     
   };
 
   ngOnInit() {
-    this.recipeService.recipeSelected.subscribe(
-      (recipe: Recipe) => this.recipe = recipe
-    );
+    
   }
 
 }
