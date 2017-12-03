f2b8e1c4ad56522a18a94d029dc81e7926f1d16f
gorgant <gorgant@hotmail.com>
2017-12-03 15:39:08 -0500

    implement skeleton for firebase authentication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
+  ngOnInit() {
+    // This expects to receive a javascript object form the backend as an argument
+    // See Firebase -> authentication -> sign-in method -> web setup for the key and domain
+    firebase.initializeApp({
+      apiKey: 'AIzaSyDnO2CU_041AmhSfzPYLJwA_D_qTiebhyc',
+      authDomain: 'udemy-ng-recipe-book-83029.firebaseapp.com'
+    });
   }
+
+}
 
 
