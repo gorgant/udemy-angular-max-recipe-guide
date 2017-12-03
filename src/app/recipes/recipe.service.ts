a723c75e5a4bd8fa4889182415be277343f0bae9
gorgant <gorgant@hotmail.com>
2017-12-03 14:09:20 -0500

    Quick tweak to include initial dummy data (which gets overridden when fetching)

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -14,23 +14,25 @@ export class RecipeService {
   constructor(private shoppingListService: ShoppingListService) { }
 
   private recipes: Recipe[] = [
-    // new Recipe(
-    //   'Tasty Schnitzel',
-    //   'A super-tasty Schnitzel - just awesome!',
-    //   'https://upload.wikimedia.org/wikipedia/commons/a/ae/Wiener-Schnitzel02.jpg',
-    //   [
-    //     new Ingredient('Meat', 1),
-    //     new Ingredient('French Fries', 20),
-    //   ]),
-    // new Recipe(
-    //   'Big Fat Burger',
-    //   'Yeah, its that good',
-    //   'https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/Hamburger_%28black_bg%29.jpg/1920px-Hamburger_%28black_bg%29.jpg',
-    //   [
-    //     new Ingredient('Buns', 2),
-    //     new Ingredient('Meat', 1),
-    //   ]),
+    new Recipe(
+      'Tasty Schnitzel',
+      'A super-tasty Schnitzel - just awesome!',
+      'https://upload.wikimedia.org/wikipedia/commons/a/ae/Wiener-Schnitzel02.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20),
+      ]),
+    new Recipe(
+      'Big Fat Burger',
+      'Yeah, its that good',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/Hamburger_%28black_bg%29.jpg/1920px-Hamburger_%28black_bg%29.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1),
+      ]),
   ];
+
+  // This overwrites the initial dummy data
   setRecipes(recipes: Recipe[]) {
     this.recipes = recipes;
     this.recipesChanged.next(this.recipes.slice());
