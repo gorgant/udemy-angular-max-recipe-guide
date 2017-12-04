01214bb407320c20e4ff41ebfa79b826460a8ad8
gorgant <gorgant@hotmail.com>
2017-12-04 06:41:38 -0500

    implement token functionality

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import * as firebase from 'firebase';
 
 export class AuthService {
+  token: string;
   // The auth() method gives us access to all the firebase auth methods
   signupUser(email: string, password: string) {
     firebase.auth().createUserWithEmailAndPassword(email, password)
@@ -12,10 +13,26 @@ export class AuthService {
   signinUser(email: string, password: string) {
     firebase.auth().signInWithEmailAndPassword(email, password)
       .then(
-        response => console.log(response)
+        response => {
+          firebase.auth().currentUser.getIdToken()
+            .then(
+              (token: string) => this.token = token
+            );
+        }
       )
       .catch(
         error => console.log(error)
       );
+  }
+
+  getToken() {
+    // This is an asynchronous call because not only does it fetch from local storage (synchronous)
+    // ... it also pings Firebase server to ensure the token hasn't expired
+    // ... if expired we'd need to implement some error handling to get the user to try again
+    firebase.auth().currentUser.getIdToken()
+      .then(
+        (token: string) => this.token = token
+      );
+    return this.token;
   }
 }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -6,6 +6,7 @@ import { Observable } from 'rxjs/Observable';
 
 import { Recipe } from './../recipes/recipe.model';
 import { RecipeService } from './../recipes/recipe.service';
+import { AuthService } from './../auth/auth.service';
 
 // Need the injectable here to activate the Http service in the constructor (make sure to import the Http into the app module as well)
 @Injectable()
@@ -13,9 +14,12 @@ export class DataStorageService {
 
   constructor(
     private http: Http,
-    private recipeService: RecipeService) {}
+    private recipeService: RecipeService,
+    private authService: AuthService) {}
 
   storeRecipes() {
+    const token = this.authService.getToken();
+
     const recipes = this.recipeService.getRecipes();
     const headers = new Headers({'Content-Type': 'application/json'});
     // This put method is stored as an observable and isn't activated until it's subscribed to
@@ -23,20 +27,21 @@ export class DataStorageService {
     // ..it will create a node with that name in the firebase database
     // For Firebase specifically, the PUT request overwrites data rather than appending it like the POST request
     return this.http.put(
-      'https://udemy-ng-recipe-book-83029.firebaseio.com/recipes.json',
+      'https://udemy-ng-recipe-book-83029.firebaseio.com/recipes.json?auth=' + token,
       recipes,
       {headers: headers});
   }
 
   getRecipes() {
+    const token = this.authService.getToken();
+
     // this mapping function ensures that if there are no ingredients, a blank array is inserted
-    return this.http.get('https://udemy-ng-recipe-book-83029.firebaseio.com/recipes.json')
+    return this.http.get('https://udemy-ng-recipe-book-83029.firebaseio.com/recipes.json?auth=' + token)
       .map(
         (response: Response) => {
           const recipes: Recipe[] = response.json();
           for (let recipe of recipes) {
             if (!recipe['ingredients']) {
-              console.log(recipe);
               recipe['ingredients'] = [];
             }
           }
