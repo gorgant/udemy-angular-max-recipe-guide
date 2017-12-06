05b577747f0099e9fb9425c97fa6ec53b355b0f0
gorgant <gorgant@hotmail.com>
2017-12-06 06:50:51 -0500

    shift services from app module to core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,18 @@
-import { CoreModule } from './core/core.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
-import { ShoppingListService } from './shopping-list/shopping-list.service';
-import { RecipeService } from './recipes/recipe.service';
-import { DataStorageService } from './shared/data-storage.service';
-import { AuthService } from './auth/auth.service';
-import { AuthGuard } from './auth/auth-guard.service';
-
+import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 import { AuthModule } from './auth/auth.module';
-import { AppRoutingModule } from './app-routing.module';
+import { CoreModule } from './core/core.module';
 
 
 @NgModule({
   declarations: [
+    // All other components are in their respective feature modules and are loaded when their route is activated
     AppComponent
   ],
   imports: [
@@ -28,13 +23,8 @@ import { AppRoutingModule } from './app-routing.module';
     AuthModule,
     CoreModule
   ],
-  providers: [
-    ShoppingListService,
-    RecipeService,
-    DataStorageService,
-    AuthService,
-    AuthGuard
-  ],
+  // Providers are all stored in the core module
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,6 +1,13 @@
+import { NgModule } from '@angular/core';
+
+import { AuthGuard } from './../auth/auth-guard.service';
+import { AuthService } from './../auth/auth.service';
+import { DataStorageService } from './../shared/data-storage.service';
+import { RecipeService } from './../recipes/recipe.service';
+import { ShoppingListService } from './../shopping-list/shopping-list.service';
+
 import { AppRoutingModule } from './../app-routing.module';
 import { SharedModule } from './../shared/shared.module';
-import { NgModule } from '@angular/core';
 
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
@@ -21,6 +28,14 @@ import { HeaderComponent } from './header/header.component';
     AppRoutingModule,
     // Need to export header bc used in app.component.html
     HeaderComponent
+  ],
+  providers: [
+    // Angular will load one instance of all of these for the app as long as this core module is loaded eagerly
+    ShoppingListService,
+    RecipeService,
+    DataStorageService,
+    AuthService,
+    AuthGuard
   ]
 })
 export class CoreModule {}
