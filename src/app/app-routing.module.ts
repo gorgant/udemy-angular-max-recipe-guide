8de35a9673db7593dc459630f92ae05f6ffa3f5a
gorgant <gorgant@hotmail.com>
2017-12-06 08:20:42 -0500

    implement pre-loading functionality

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 import { AuthGuard } from './auth/auth-guard.service';
 import { HomeComponent } from './core/home/home.component';
@@ -16,7 +16,10 @@ const appRoutes: Routes = [
   imports: [
     // GCR: Use this hash route version if getting 404 error in deployment (see "understanding location strategies")
     // RouterModule.forRoot(appRoutes, {useHash: true})
-    RouterModule.forRoot(appRoutes)
+
+    // This preloading strategy preloads selected modules (in this case all) after the initial load takes place
+    // ... note that if an AuthGuard is present on a route, then it doesn't pre-load (need to remove to get it to work)
+    RouterModule.forRoot(appRoutes, {preloadingStrategy: PreloadAllModules} )
   ],
   exports: [RouterModule]
 
